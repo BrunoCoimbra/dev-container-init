6ef75ea675b5364b4c550d1c9a0b4752d876dd6c
Bruno Coimbra <coimbra@live.com>
2024-06-05 13:39:46 -0500

    Initial commit

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+if [[ $? -ne 0 ]]; then
+  echo "Failed to install oh-my-zsh"
+  exit 1
+fi
+
+cp templates/.zshrc ~/.zshrc
+cp templates/container.zsh-theme ~/.oh-my-zsh/custom/themes/container.zsh-theme
+
+cd ~/.oh-my-zsh/custom/plugins || exit 1
+git clone https://github.com/cedi/meaningful-error-codes.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
